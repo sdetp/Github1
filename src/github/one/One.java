c2c23ba2c8a0ac360ccf5b73788facc20b0f3861
Cheryl Thai <94874067+sdetp@users.noreply.github.com>
2021-11-24 20:44:00 -0500

    Update One.java

--- src/github/one/One.java
+++ src/github/one/One.java
@@ -7,6 +7,9 @@ public class One {
 		System.out.println("practicing...");
 		
 		System.out.println("adding one more output message");
+		
+		System.out.println("I have changed something");
+		//this is random message
 	}
 	
 	
